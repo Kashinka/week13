3461de11b3c05e67d8d529ee46f836aaeb53bbdc
Kashinka <azalena@yandex.ru>
2022-10-09 19:34:37 +0300

    Added four functions

--- calc.js
+++ calc.js
@@ -1,2 +1,33 @@
 /* Сделайте простой калькулятор на 4 действия (сложение, вычитание, деление, умножение). 4 кнопки, по нажатию которые запрашиваются два числа и выдается результат их вычисления, соответствующий действию на кнопке. */
 
+function showMessagePlus() {
+  
+    let name = prompt('Как тебя зовут?', "Имя");
+    
+    alert('Привет, ' + name);
+    
+    }
+
+function showMessageMinus() {
+  
+    let name = prompt('Как тебя зовут?', "Имя");
+        
+    lert('Привет, ' + name);
+        
+    }
+    
+function showMessageDivide() {
+  
+    let name = prompt('Как тебя зовут?', "Имя");
+            
+    alert('Привет, ' + name);
+            
+    }
+
+function showMessageMultiply() {
+  
+    let name = prompt('Как тебя зовут?', "Имя");
+                
+    alert('Привет, ' + name);
+                
+    }
